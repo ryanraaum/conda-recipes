79c786f258f36eafd98a46b49c73d9d080cbaec3
Ryan Raaum <ryan.raaum@lehman.cuny.edu>
2019-02-14 18:16:02 -0500

    reverting to fs 2.1.3 source build

--- finestructure/build.sh
+++ finestructure/build.sh
@@ -2,13 +2,15 @@
 
 mkdir -p $PREFIX/bin
 
-if [[ $(uname -s) == Darwin ]]; then
-	cp fs_mac $PREFIX/bin/fs
-else
-	cp fs_linux_glibc2.3 $PREFIX/bin/fs
-fi
+./configure --prefix=$PREFIX && make && make install
 
-# support scripts
-cp {beagle2chromopainter.pl,chromopainter2chromopainterv2.pl,convertrecfile.pl} $PREFIX/bin/
-cp {impute2chromopainter.pl,makeuniformrecfile.pl,msms2cp.pl,phasescreen.pl} $PREFIX/bin/
-cp {phasesubsample.pl,plink2chromopainter.pl} $PREFIX/bin/
+# if [[ $(uname -s) == Darwin ]]; then
+# 	cp fs_mac $PREFIX/bin/fs
+# else
+# 	cp fs_linux_glibc2.3 $PREFIX/bin/fs
+# fi
+
+# # support scripts
+# cp {beagle2chromopainter.pl,chromopainter2chromopainterv2.pl,convertrecfile.pl} $PREFIX/bin/
+# cp {impute2chromopainter.pl,makeuniformrecfile.pl,msms2cp.pl,phasescreen.pl} $PREFIX/bin/
+# cp {phasesubsample.pl,plink2chromopainter.pl} $PREFIX/bin/
